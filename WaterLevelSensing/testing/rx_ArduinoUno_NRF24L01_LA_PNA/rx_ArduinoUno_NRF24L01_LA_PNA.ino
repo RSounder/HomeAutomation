6e3e244af0930ae44483fb3ee75cb61ef5245eaf
RSounder <sounderajendran@gmail.com>
2022-04-07 00:25:20 +0530

    Created folder structure for water sensing module. Added test scripts for tx+rx

---
+++ WaterLevelSensing/testing/rx_ArduinoUno_NRF24L01_LA_PNA/rx_ArduinoUno_NRF24L01_LA_PNA.ino
@@ -0,0 +1,53 @@
+#include <RH_NRF24.h>
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2,3, 4, 5, 6, 7);
+
+RH_NRF24 nrf24;
+ 
+void setup() 
+{
+  Serial.begin(9600);
+  while (!Serial) ;
+  if (!nrf24.init())
+    Serial.println("initialization failed");                           
+  if (!nrf24.setChannel(1))
+    Serial.println("Channel Set failed");
+  if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm))
+    Serial.println("RF set failed");    
+
+    lcd.begin(16, 2);
+    lcd.setCursor(0, 0);
+    lcd.print("   Welcome to        ");
+    lcd.setCursor(0, 1);
+    lcd.print(" Circuit Digest      ");
+
+    delay(1000);
+    lcd.clear();
+}
+ 
+void loop()
+{
+  if (nrf24.available())
+  {
+    uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    if(nrf24.recv(buf, &len))
+    {
+      Serial.print("Received: ");
+      Serial.println((char*)buf);
+      String data=String((char*)buf);
+      lcd.setCursor(0,0);
+      lcd.print(data.substring(0,10));
+      lcd.setCursor(11,0);
+      lcd.print(data.substring(20,24));
+      lcd.setCursor(0,1);
+      lcd.print("Time:");
+      lcd.setCursor(6,1);
+      lcd.print(data.substring(11,19));
+    }
+    else
+    {
+      Serial.println("recv failed");
+    }
+  }
+}
---
+++ WaterLevelSensing/testing/tx_NodeMCU_NRF24L01_LA_PNA/tx_NodeMCU_NRF24L01_LA_PNA.ino
@@ -0,0 +1,50 @@
+#include <RH_NRF24.h>
+#include <ESP8266WiFi.h>
+#include <time.h>
+
+const char* ssid = "Stark";
+const char* password = "eachday97";
+
+RH_NRF24 nrf24(2,4);
+
+void setup() 
+{
+  Serial.begin(9600);
+  while (!Serial);      
+  if (!nrf24.init())
+    Serial.println("initialization failed");
+  if (!nrf24.setChannel(1))
+    Serial.println("Channel set failed");
+  if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm))
+    Serial.println("RF set failed");    
+
+   Serial.setDebugOutput(true);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("\nConnecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  configTime(11*1800, 0, "pool.ntp.org", "time.nist.gov");
+  Serial.println("\nWaiting for time");
+  while (!time(nullptr)) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("");
+}
+ 
+void loop()
+{
+  Serial.println("Sending data to receiver");
+  time_t now = time(nullptr);
+  String timestring = String(ctime(&now));
+  uint8_t dataArray[timestring.length()];
+  timestring.getBytes(dataArray,timestring.length());
+  nrf24.send(dataArray,sizeof(dataArray));
+  nrf24.waitPacketSent();
+  delay(1000);
+}
